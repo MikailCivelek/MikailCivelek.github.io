d28ac12c14a5a6d66e51ea915937ac1d9c930326
MikailCivelekPXL <Mikail.civelek@student.pxl.be>
2022-04-29 09:15:49 +0200

    trying again

---
+++ dist/_nuxt/static/1651216519/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freflectieverslagen\u002Fprogrammeerwedstrijd","\u002Freflectieverslagen\u002Fstudybuddy","\u002Freflectieverslagen\u002Finnovatieroute"]})
